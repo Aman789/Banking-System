ffc4fd87315ba9398ba264f41394140a43f61c38
amsin <asingh78980@gmail.com>
2025-12-16 16:09:30 -0500

    Added functionality for transfers and account status

--- src/main/java/com/bankingsystem/model/AccountService.java
+++ src/main/java/com/bankingsystem/model/AccountService.java
@@ -24,4 +24,50 @@ public class AccountService {
             System.out.println("Not enough funds");
         }
     }
+
+    public void transaction(Account fromAcc, Account toAcc, BigDecimal ammount){
+        BigDecimal currBalance = fromAcc.getBalance();
+
+        if (currBalance.compareTo(ammount) >= 0) {
+            fromAcc.setBalance(fromAcc.getBalance().subtract(ammount));
+            //call withdraw function to take out money from account
+            withdraw(fromAcc, ammount);
+
+            //call deposit function to deposit money to account
+            deposit(toAcc,ammount);
+        }
+        else {
+            System.out.println("Not enough funds to transfer");
+        }
+
+    }
+
+    public void closeAccount(Account account){
+        if (account.getStatus() == AccountStatus.CLOSED){
+            System.out.println("Account is closed");
+        }
+        else{
+            account.setStatus(AccountStatus.CLOSED);
+        }
+    }
+
+    public void suspendAccount(Account account){
+        if (account.getStatus() == AccountStatus.CLOSED){
+            System.out.println("Account is closed");
+        } else if (account.getStatus() == AccountStatus.SUSPENDED) {
+            System.out.println("Account is Suspended");
+        } else{
+            account.setStatus(AccountStatus.SUSPENDED);
+        }
+    }
+
+    public void activateAccount(Account account){
+        if (account.getStatus() == AccountStatus.ACTIVE){
+            System.out.println("Account is already active");
+        }  else{
+            account.setStatus(AccountStatus.ACTIVE);
+        }
+    }
+
+
 }
